2ff8ff0801cdeddf9c87009b7d0dfa35eeb42271
Paulo Silveira <49129267+asjr-p@users.noreply.github.com>
2023-05-09 21:53:27 -0300

    Update Eleitor and Candidato classes

--- scripts/Candidato.js
+++ scripts/Candidato.js
@@ -4,9 +4,60 @@ class Candidato {
       this.partido = partido;
       this.votos = 0;
     }
+  }
   
-    exibirDados() {
-      console.log(`Nome: ${this.nome} | Partido: ${this.partido} | Votos: ${this.votos}`);
+  class CandidatoController {
+    constructor() {
+      this.candidatos = [];
+    }
+  
+    adicionarCandidato(nome, partido) {
+      const candidato = new Candidato(nome, partido);
+      this.candidatos.push(candidato);
+    }
+  
+    exibirCandidatos() {
+      console.log('Candidatos:');
+      this.candidatos.forEach((candidato) => {
+        console.log(`- ${candidato.nome} (${candidato.partido}, Votos: ${candidato.votos})`);
+      });
+    }
+  
+    atualizarCandidato(nomeAntigo, nomeNovo, partidoNovo) {
+      const candidato = this.candidatos.find((candidato) => candidato.nome === nomeAntigo);
+  
+      if (!candidato) {
+        throw new Error(`Não foi encontrado nenhum candidato com o nome ${nomeAntigo}.`);
+      }
+  
+      candidato.nome = nomeNovo;
+      candidato.partido = partidoNovo;
+    }
+  
+    excluirCandidato(nome) {
+      const index = this.candidatos.findIndex((candidato) => candidato.nome === nome);
+  
+      if (index === -1) {
+        throw new Error(`Não foi encontrado nenhum candidato com o nome ${nome}.`);
+      }
+  
+      this.candidatos.splice(index, 1);
     }
   }
+  
+  // Usage example
+  const candidatoController = new CandidatoController();
+  
+  candidatoController.adicionarCandidato('João', 'Partido A');
+  candidatoController.adicionarCandidato('Maria', 'Partido B');
+  
+  candidatoController.exibirCandidatos();
+  
+  candidatoController.atualizarCandidato('João', 'José', 'Partido C');
+  
+  candidatoController.exibirCandidatos();
+  
+  candidatoController.excluirCandidato('Maria');
+  
+  candidatoController.exibirCandidatos();
   
--- scripts/Eleitor.js
+++ scripts/Eleitor.js
@@ -15,3 +15,57 @@ class Eleitor {
     }
   }
   
+  class EleitorController {
+    constructor() {
+      this.eleitores = [];
+    }
+  
+    adicionarEleitor(nome, idade) {
+      const eleitor = new Eleitor(nome, idade);
+      this.eleitores.push(eleitor);
+    }
+  
+    exibirEleitores() {
+      console.log('Eleitores:');
+      this.eleitores.forEach((eleitor) => {
+        console.log(`- ${eleitor.nome} (Idade: ${eleitor.idade}, Votou: ${eleitor.votou})`);
+      });
+    }
+  
+    atualizarEleitor(nomeAntigo, nomeNovo, idadeNova) {
+      const eleitor = this.eleitores.find((eleitor) => eleitor.nome === nomeAntigo);
+  
+      if (!eleitor) {
+        throw new Error(`Não foi encontrado nenhum eleitor com o nome ${nomeAntigo}.`);
+      }
+  
+      eleitor.nome = nomeNovo;
+      eleitor.idade = idadeNova;
+    }
+  
+    excluirEleitor(nome) {
+      const index = this.eleitores.findIndex((eleitor) => eleitor.nome === nome);
+  
+      if (index === -1) {
+        throw new Error(`Não foi encontrado nenhum eleitor com o nome ${nome}.`);
+      }
+  
+      this.eleitores.splice(index, 1);
+    }
+  }
+  
+  const eleitorController = new EleitorController();
+  
+  eleitorController.adicionarEleitor('João', 25);
+  eleitorController.adicionarEleitor('Maria', 35);
+  
+  eleitorController.exibirEleitores();
+  
+  eleitorController.atualizarEleitor('João', 'José', 30);
+  
+  eleitorController.exibirEleitores();
+  
+  eleitorController.excluirEleitor('Maria');
+  
+  eleitorController.exibirEleitores();
+  
